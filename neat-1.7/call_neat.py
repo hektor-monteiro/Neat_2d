0e1ec114b2fe6596b92eab1577c859419338a0e8
hektor-monteiro <hektor.monteiro@gmail.com>
2015-12-17 20:26:29 -0200

    added bit to read output file and parse it.

--- neat-1.7/call_neat.py
+++ neat-1.7/call_neat.py
@@ -1,12 +1,31 @@
+#from ConfigNumParser import *
+import numpy as np
+from subprocess import call
+
+##################################
 # Program to call and run Neat
 # !!!! should be turn to a function or module in the future
 
-from subprocess import call
-
 dir = '/home/hmonteiro/Dropbox/work/devel/Neat_2d/neat-1.7/'
-input_file = dir+"examples/ngc6543.dat"
+input_file = dir+'examples/ngc6543.dat'
 flags = " "
 
 call('./neat -i'+' '+input_file+' '+flags, shell=True)
+
+
+###################################
+# Parse output file
+
+f=open(input_file+'_results',"r")
+txt=f.read() #all text is in txt
+f.close()
+
+splitedlines = txt.splitlines()
+
+nlines = len(splitedlines)
+
+for i in range(0,nlines-1):
+    print(splitedlines[i])
+    print(len(splitedlines[i].split()))
 
 
